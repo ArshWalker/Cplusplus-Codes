a109bd0414b21e95a1a38ee728770448776b6fc9
ArshWalker <arshwalker7@gmail.com>
2021-06-16 03:16:55 +0530

    Patterns added

---
+++ Patterns/hollowrectpattern.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows, col;
+    cout << "Enter the no. of rows: ";
+    cin >> rows;
+    cout << "Enter the no. of columns: ";
+    cin >> col;
+
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= col; j++)
+        {
+            if (i == 1 || i == rows || j == 1 || j == col)
+            {
+                cout << "* ";
+            }
+            else
+            {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
