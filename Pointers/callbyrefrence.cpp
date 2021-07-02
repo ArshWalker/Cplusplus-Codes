55324809bcb2b6c0ac71d12aef6674371cf56b4c
ArshWalker <arshwalker7@gmail.com>
2021-07-03 04:05:40 +0530

    call by reference in Swaping added

---
+++ Pointers/callbyrefrence.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int *a, int *b)
+{
+     int temp = *a;
+     *a = *b;
+     *b = temp;
+}
+int main()
+{
+     int a = 9, b = 1;
+     int *aptr = &a;
+     int *bptr = &b;
+
+     swap(aptr, bptr);
+     cout << a << " " << b << endl;
+
+     return 0;
+}
